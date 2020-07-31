f11a4486b4fe4f8f3d2d93bd434e9f5ae800c131
IntiDev <yeseniase@outlook.it>
2020-07-30 21:25:15 -0500

    [WIP] react base example

---
+++ 7.react/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: {
+        home: path.resolve(__dirname, 'src/index.js'),
+    },
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js'//[name] hace referencia a los nombres de entry (home, precios, contacto)
+    },
+    devServer: {
+        hot: true,
+        open: true, //para abrir un tab en el navegador y no tener que abrir manualmente
+        port: 9000,
+    },
+    module: { //Aquí van los loaders
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: '/node_modules/',
+            },
+            {
+                test: /\.css$/, //tipo de archivo
+                use: [
+                     'style-loader',//CSS: ENTORNO DE DESARROLLO (más rápido)
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'webpack-dev-server',
+            template: path.resolve(__dirname, 'index.html')
+        }),
+    ]
+}
